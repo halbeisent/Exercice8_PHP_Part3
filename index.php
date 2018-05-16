752ae403cfeb8aa03d69877271db4d6d3f80a20f
halbeisent <dev@halbeisen.fr>
2018-05-16 16:10:08 +0200

    Exercice 8 Partie 3 PHP Terminé et Corrigé

--- index.php
+++ index.php
@@ -6,14 +6,9 @@
     <title>Exercice 8 Partie 3 PHP</title>
   </head>
   <body>
-    <?php
-    // On initialise la variable $count à 200
-          $count = 200;
-          while ($count >= 0) { //Tant que sa valeur est supérieure ou égale à 0,
-            echo 'Enfin!!! <br />'; //On affiche ce message,
-            $count-=12; //Puis on décrémente la variable de 12 (équivalent à $count = $count+12)
-            var_dump($count); //Utilisé pour diagnostic
-          }
-          ?>
+      <?php
+      // Tant que la valeur de $count est supérieure à 0, on affiche le message ci-dessous:
+      for ($count = 200; $count >= 0; $count-=12) { ?>
+        <p><?= 'Enfin!!!!'; }?></p>
   </body>
 </html>
